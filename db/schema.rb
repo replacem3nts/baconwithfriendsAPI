e5228679f268fe992222fe572e68bbd045b04530
jackmt9 <jackmt9@gmail.com>
2020-05-24 16:42:22 -0400

    Added slug attribute to GameRecord model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_24_202111) do
+ActiveRecord::Schema.define(version: 2020_05_24_204128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_05_24_202111) do
   create_table "game_records", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.integer "rank"
+    t.integer "slug"
     t.index ["user_id"], name: "index_game_records_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Users
+
+user1 = User.create(username: "user1", password: "123")
+user2 = User.create(username: "user2", password: "password")
+user3 = User.create(username: "user3", password: "pass")
+
+# GameRecords
+
+GameRecord.create(user: user1, rank: 3)
+
