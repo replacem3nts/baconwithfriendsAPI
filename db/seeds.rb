9c459bcb887d6f46830265ff83500e6ba977834d
jackmt9 <jackmt9@gmail.com>
2020-05-24 16:49:51 -0400

    Seed Data Created close #5

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@ user2 = User.create(username: "user2", password: "password")
 user3 = User.create(username: "user3", password: "pass")
 
 # GameRecords
-
-GameRecord.create(user: user1, rank: 3)
+slug = rand(1..100000)
+GameRecord.create(user: user1, rank: 3, slug: slug)
+GameRecord.create(user: user2, rank: 5, slug: slug)
+GameRecord.create(user: user3, rank: 8, slug: slug)
 
