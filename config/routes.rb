0b1f2df19958eb3e29cf30b4aba05fc3b9bbbfad
jackmt9 <jackmt9@gmail.com>
2020-05-27 15:24:39 -0400

    Created Rooms_controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
 
+  # users_controller routing
   get '/login', to: 'users#login'
   get '/profile', to: 'users#profile'
   post '/create_account', to: 'users#create'
   patch '/change_username', to: 'users#update'
   delete '/delete', to: 'users#destroy'
+
+  # rooms_controller routing
+  post '/create_room', to: 'rooms#create'
+  delete '/delete_room/:id', to: 'rooms#destroy'
+
+  # game_record routing
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
