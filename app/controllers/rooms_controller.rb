a2edc0b5f461ac4b44ab0408dba2a760fc94f0c7
jackmt9 <jackmt9@gmail.com>
2020-05-29 11:00:10 -0400

    room_params in rooms_controller updated

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -54,7 +54,7 @@ class RoomsController < ApplicationController
   private
 
   def room_params
-    params.permit(:slug, :name)
+    params.permit(:roomopen, :lastanswer, :actoranswer, :whoseturn, :lastrank)
   end
 
 end
